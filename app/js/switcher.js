431781295dec417b73b32060588acb5f6840842d
deRerumNatura <pollindrom@gmail.com>
2017-07-30 19:14:25 +0300

    switcher

--- app/js/switcher.js
+++ app/js/switcher.js
@@ -4,12 +4,12 @@ $( document ).ready(function() {
 
 $('#menu_switcher').click(function(){
 		if(menu.css("opacity") < '0.8') {
-			menu.animate({"opacity": '0.8', "top": '10px'}, 500);
+			menu.animate({/*"opacity": '0.8',*/ "top": '10px'}, 500);
 			lines.addClass('ms_lines_closed');
 			lines.removeClass('sw-all');
 		}
 		else {
-			menu.animate({"top": '50px', "opacity": '0'}, 400);	
+			menu.animate({/*"top": '50px',*/ "opacity": '0'}, 400);	
 			lines.removeClass('ms_lines_closed');
 			lines.addClass('sw-all');
 		}
